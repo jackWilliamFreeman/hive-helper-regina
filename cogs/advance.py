fb12f3f873158a7b715e7373c620b8cf86f0766f
William Freeman <wfreeman@fmgl.com.au>
2022-06-13 19:35:03 +0800

    major changes

--- cogs/advance.py
+++ cogs/advance.py
@@ -1,17 +1,31 @@
+from turtle import title
 import discord
 from discord.ext import commands
+from discord.commands import OptionChoice
 import random
 from insults import get_long_insult
+import pandas as pd
 
 import os
 import sys
+
+global roll
+global advancement_table
+second_roll = 0
+global options
 
 SCRIPT_DIR = os.path.dirname(os.path.abspath(__file__))
 sys.path.append(os.path.dirname(SCRIPT_DIR))
 
 from insults import get_long_insult
+regina_url = 'https://scontent.xx.fbcdn.net/v/t1.15752-9/278403172_399692048829552_6640220989778099445_n.jpg?stp=dst-jpg_s403x403&_nc_cat=101&ccb=1-5&_nc_sid=aee45a&_nc_ohc=fp1v8cyJAJwAX8OItsD&_nc_ad=z-m&_nc_cid=0&_nc_ht=scontent.xx&oh=03_AVJGPV02ajRAuuVrZxJxjwaIpQNKrbd1MTu_QNLywsnqsw&oe=6289995B'
 
 options = []
+
+advancement_style = [
+        OptionChoice(name="Advancing a Ganger", value="ganger"), #  Value must be a string.
+        OptionChoice(name="Advancing a Juve, Leader, Champ or Specialist", value="other") #  Value must be a string.
+    ]
 
 
 i = 10
@@ -27,21 +41,6 @@ while i >= -10:
     options.append(option)
     i-=1
 
-class MySecondView(discord.ui.View):
-    @discord.ui.select( # the decorator that lets you specify the properties of the select menu
-        placeholder = "Modify Second Roll??", # the placeholder text that will be displayed if nothing is selected
-        min_values = 1, # the minimum number of values that must be selected by the users
-        max_values = 1, # the maxmimum number of values that can be selected by the users
-        options = options,
-        disabled=False,
-        row=0
-    )
-    async def select_callback(self, select, interaction): # the function called when the user is done selecting options
-        select.disabled=True
-        
-        await interaction.response.edit_message(view=self)
-        await interaction.message.reply("good roll cnt")
-
 
 class MyView(discord.ui.View):
     @discord.ui.select( # the decorator that lets you specify the properties of the select menu
@@ -53,16 +52,73 @@ class MyView(discord.ui.View):
     )
 
     async def select_callback(self, select, interaction): # the function called when the user is done selecting options
+        selected_value = select.values[0]
+        global roll
+        roll = roll + int(selected_value)
+        if roll > 12 or roll < 1:
+            
+            select.disabled=True
+            await interaction.response.edit_message(view=self)
+            await gctx.send(f'YOU NUMPTY! YOU MUST CHOOSE AN XP MODIFICATION THAT KEEPS IT WITHIN 12 AND 1...TRY AGAIN')
+            return
+
+        #work out where we are currently after previous selections
+        current_advancement = advancement_table.loc[advancement_table['roll'] == roll]
+        new_advancement_description = current_advancement['description'].values[0]
+        global second_roll
+        if current_advancement['has_another_roll'].values[0]: 
+                dice_size = current_advancement['dice_size'].values[0]
+                #TODO MAKE THE DICE GLOBAL FOR REUSE AND MODIFICATION
+                second_roll = random.randint(1, dice_size)
+                if second_roll % 3 == 0:
+                    mod_roll = 2
+                else:
+                    mod_roll = 1
+                selected_option = current_advancement[f'option_{mod_roll}'].values[0]
+        else:
+                selected_option = new_advancement_description
+
+        #they changed the roll
+        if selected_value != '0':
+            await interaction.message.edit(f"You have changed the roll to {roll} by adding {int(selected_value)}, this means {new_advancement_description}")
+            select.disabled=True
+            await interaction.response.edit_message(view=self)
+
+            await interaction.followup.send(f'nice choice your ganger has gained: {selected_option}')
+        
+        #they didnt change the roll and can modify further
+        else:
+            select.disabled=True
+            await interaction.message.edit(f'Now you have selected to remain with {new_advancement_description} and have rolled a {second_roll}, you can modify this with xp if needed')
+            await interaction.response.edit_message(view=MySecondView())
+
+class MySecondView(discord.ui.View):
+    @discord.ui.select( # the decorator that lets you specify the properties of the select menu
+        placeholder = "Modify Second Roll??", # the placeholder text that will be displayed if nothing is selected
+        min_values = 1, # the minimum number of values that must be selected by the users
+        max_values = 1, # the maxmimum number of values that can be selected by the users
+        options = options,
+        disabled=False,
+        row=0
+    )
+    async def select_callback(self, select, interaction): # the function called when the user is done selecting options
         select.disabled=True
         selected_value = select.values[0]
-        if selected_value != '0':
-            await interaction.message.edit("Selection Complete")
-            await interaction.response.edit_message(view=self)
-            await interaction.followup.send('nice choice')
-            
+        current_advancement = advancement_table.loc[advancement_table['roll'] == roll]
+        global second_roll
+        if selected_value != 0:
+            second_roll = second_roll + int(selected_value)
+        if second_roll % 3 != 0:
+            mod_roll = 2
         else:
-            await interaction.message.edit("Now do you want to spend xp on the second roll?")
-            await interaction.response.edit_message(view=MySecondView())
+            mod_roll = 1
+        if current_advancement['has_another_roll'].values[0]:
+            selected_option = current_advancement[f'option_{mod_roll}'].values[0]
+        else:
+            selected_option = current_advancement['description'].values[0]
+
+        await interaction.response.edit_message(view=self)
+        await interaction.message.reply(f'you got {selected_option}')
 
 class advance(commands.Cog): # create a class for our cog that inherits from commands.Cog
     # this class is used to create a cog, which is a module that can be added to the bot
@@ -74,10 +130,35 @@ class advance(commands.Cog): # create a class for our cog that inherits from com
     # pycord will figure out the types for you
     async def advance(
     self,
-    ctx
+    ctx,
+    advancement_style: discord.Option(str, "What type of Crate was it?", choices=advancement_style)
     ):
+        global gctx
+        gctx = ctx
+        if advancement_style == 'ganger':
+            advancement_file = 'assets\Advance_gangers.csv'
+        else:
+            advancement_file = 'assets\Advance_Leaders_Champs_Spec_Juves.csv'
+        global advancement_table
+        advancement_table = pd.read_csv(advancement_file)
+
+        embed = discord.Embed(
+                title="Advancement Table",
+                description=f"Check out the table below:",
+                color=discord.Colour.blurple(), # Pycord provides a class with default colors you can choose from
+                )
+        embed.add_field(name="Below are the rolls and their options", value="use to modify your rolls if required")
+        for index, row in advancement_table.iterrows():
+            embed.add_field(name=row['roll'], value=row['description'], inline=False)
+        embed.set_author(name="Hive Helper Regina", icon_url=regina_url)
+        embed.set_thumbnail(url=regina_url)
+        global roll
         roll=random.randint(2,12)
-        await ctx.respond(f"Here is the Advance table, you rolled a {roll}",view=MyView(timeout=25))
+
+        current_advance = advancement_table.loc[advancement_table['roll'] == roll]['description'].values[0]
+
+        await ctx.send("here is the advancement table:", embed=embed)
+        await ctx.send(f"You have rolled a {roll}, currently that means {current_advance}\r\n You can use XP to modify the roll",view=MyView(timeout=25))
     
 
 def setup(bot): # this is called by Pycord to setup the cog
--- cogs/loot.py
+++ cogs/loot.py
@@ -13,6 +13,13 @@ sys.path.append(os.path.dirname(SCRIPT_DIR))
 
 from insults import get_long_insult
 
+class loot_results():
+    def __init__(self, description, has_secondary_text, second_roll, secondary_text):
+        self.description = description
+        self.has_secondary_text = has_secondary_text
+        self.second_roll = second_roll
+        self.secondary_text = secondary_text
+
 regina_url = 'https://scontent.xx.fbcdn.net/v/t1.15752-9/278403172_399692048829552_6640220989778099445_n.jpg?stp=dst-jpg_s403x403&_nc_cat=101&ccb=1-5&_nc_sid=aee45a&_nc_ohc=fp1v8cyJAJwAX8OItsD&_nc_ad=z-m&_nc_cid=0&_nc_ht=scontent.xx&oh=03_AVJGPV02ajRAuuVrZxJxjwaIpQNKrbd1MTu_QNLywsnqsw&oe=6289995B'
 
 class loot(commands.Cog): # create a class for our cog that inherits from commands.Cog
@@ -22,9 +29,9 @@ class loot(commands.Cog): # create a class for our cog that inherits from comman
         self.bot = bot
 
     loot_crate_type = [
-        OptionChoice(name="Gold", value="gold"), #  Value must be a string.
-        OptionChoice(name="Silver", value="silver"), #  Value must be a string.
-        OptionChoice(name="Vox Comms", value="vox Comms") #  Value must be a string.
+        OptionChoice(name="Gold", value="Gold"), #  Value must be a string.
+        OptionChoice(name="Silver", value="Silver"), #  Value must be a string.
+        OptionChoice(name="Vox Comms", value="Vox") #  Value must be a string.
     ]
 
     opening_method = [
@@ -47,78 +54,41 @@ class loot(commands.Cog): # create a class for our cog that inherits from comman
         ten_roll = random.randint(1,6)*10
         roll = single_roll+ten_roll
 
-        primary, secondary = get_loot(roll, crate_type)
-
-        title = primary['title'].values[0]
-        description = primary['description'].values[0]
-        
-        if primary['roll_required'].values[0] == 0:
-            
-            embed = discord.Embed(
+        loot_results = await get_loot_results(roll, crate_type, ctx)
+        text = loot_results.description
+        if loot_results.has_secondary_text:
+            text = text + f'\r\nYou then rolled a **{loot_results.second_roll}** and won: **{loot_results.secondary_text}**'
+        embed = discord.Embed(
                 title="Loot Roll!",
                 description=f"Congrats, you rolled a **{roll}**",
                 color=discord.Colour.blurple(), # Pycord provides a class with default colors you can choose from
                 )
-            embed.add_field(name=title, value=description, inline=False)
-            embed.set_footer(text=f"Now sod off you {get_long_insult()}") # footers can have icons too
-            embed.set_author(name="Hive Helper Regina", icon_url=regina_url)
-            embed.set_thumbnail(url=regina_url)
-            embed.set_image(url="https://i.pinimg.com/736x/33/43/6e/33436ed730d351a6278d9fd2940af70d--warhammer-k-weapons.jpg")
+        embed.add_field(name='You won:', value=text, inline=False)
+        embed.set_footer(text=f"Now sod off you {get_long_insult()}") # footers can have icons too
+        embed.set_author(name="Hive Helper Regina", icon_url=regina_url)
+        embed.set_thumbnail(url=regina_url)
+        embed.set_image(url="https://i.pinimg.com/736x/33/43/6e/33436ed730d351a6278d9fd2940af70d--warhammer-k-weapons.jpg")
+        await ctx.respond(embed=embed)
 
-            await ctx.respond(embed=embed)
-        else:
-            secondary_treasure = secondary['value'].values[0]
-            embed = discord.Embed(
-                title="Loot Roll!",
-                description=f"Congrats, you rolled a **{roll}**",
-                color=discord.Colour.blurple(), # Pycord provides a class with default colors you can choose from
-                )
-            embed.add_field(name=title, value=f"-------------------------------------------\r\nyou win: **{secondary_treasure}**", inline=False)
-            embed.set_footer(text=f"Now sod off you {get_long_insult()}") # footers can have icons too
-            embed.set_author(name="Hive Helper Regina", icon_url=regina_url)
-            embed.set_thumbnail(url=regina_url)
-            embed.set_image(url="https://i.pinimg.com/736x/33/43/6e/33436ed730d351a6278d9fd2940af70d--warhammer-k-weapons.jpg")
 
-            await ctx.respond(embed=embed)
 
-def get_loot(roll, crate_type):
-    primary_loot = get_primary_loot(roll, crate_type)
-    secondary_loot = ''
-    if primary_loot['roll_required'].values[0] == 1:
-        dice_size = primary_loot['dice_size'].values[0]
-        has_range = primary_loot['has_range'].values[0]
-        secondary_loot = get_randomn_loot(crate_type, roll, has_range, dice_size)
 
-    return primary_loot, secondary_loot
-
-def get_primary_loot(roll, crate_type):
-    df = pd.read_csv("assets/loot_rolls.csv", encoding='cp1252')
-
-    selected = df.loc[
-        (df['roll'] == roll)
-        & (df['crate_type'] == crate_type)
-    ]
-
-    return selected
-
-def get_randomn_loot(crate_type, roll, has_range, dice_size):
-    df = pd.read_csv("assets/loot_rolls_secondary.csv", encoding='cp1252')
-
-    dice_roll = random.randint(1,dice_size)
-
-    if has_range:
-        selected = df.loc[
-        (df['roll'] == roll)
-        & (df['crate_type'] == crate_type) 
-        & (df['low_range'] <= dice_roll)
-        & (df['high_range'] >= dice_roll)]
-    else:
-        selected = df.loc[
-        (df['roll'] == roll)
-        & (df['crate_type'] == crate_type) 
-        & (df['absolute'] == dice_roll)]
-
-    return selected
+async def get_loot_results(roll, crate_type, ctx):
+    try:
+        crate_file = f'assets\{crate_type}_loot_crates.csv'
+        df = pd.read_csv(crate_file, encoding='cp1252')
+        dice = 0
+        result = df.loc[df['roll'] == roll]
+        description = result['description'].values[0]
+        has_second_roll = result['has_another_roll'].values[0]
+        secondary_text = ''
+        if has_second_roll:
+            dice = random.randint(1,result['dice_size'].values[0])
+            secondary_text = result[f'option_{dice}'].values[0]
+        rolled_result = loot_results(description,has_second_roll, dice, secondary_text)
+        return rolled_result
+    except Exception as e:
+        await ctx.respond(f'Somefink went wrong, you rolled a {roll} and if you had a second roll it was a {dice} (if it was a 0 there was no second roll needed), error message was {str(e)}')
 
 def setup(bot): # this is called by Pycord to setup the cog
     bot.add_cog(loot(bot)) # add the cog to the bot
