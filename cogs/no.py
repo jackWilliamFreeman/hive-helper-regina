b1ee2777905e811dc8515ecf24b2f948bbb14d50
jackWilliamFreeman <wfreeman@fmgl.com.au>
2022-08-08 15:36:49 +0800

    added thangs

--- cogs/no.py
+++ cogs/no.py
@@ -1,4 +1,7 @@
+from importlib.metadata import files
 from discord.ext import commands
+import discord
+import os
 
 class no(commands.Cog): # create a class for our cog that inherits from commands.Cog
     # this class is used to create a cog, which is a module that can be added to the bot
@@ -12,8 +15,10 @@ class no(commands.Cog): # create a class for our cog that inherits from commands
     self,
     ctx
     ):
-
-        await ctx.respond("fuck off brad")
+        cwd = os.getcwd()
+        file = 'assets/images/no.jpg'
+        location = os.path.join(cwd,file)
+        await ctx.respond("No You!", files = [discord.File(location)])
     
 def setup(bot): # this is called by Pycord to setup the cog
     bot.add_cog(no(bot)) # add the cog to the bot
